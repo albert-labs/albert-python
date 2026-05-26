5d250a2d6f5a5389f14d2f2ea79f2e6b10f3a4fb
Ventura Rivera <venturaxrivera@gmail.com>
2026-05-26 13:24:09 -0700

    fix(HazardSymbol): adding docstring

--- src/albert/resources/hazards.py
+++ src/albert/resources/hazards.py
@@ -6,6 +6,18 @@ from albert.core.shared.models.base import EntityLinkWithName
 
 
 class HazardSymbolStatus(str, Enum):
+    """The status of a HazardSymbol.
+
+    Attributes
+    ----------
+    ACTIVE : str
+        The hazard symbol is fully operational and visible in normal operations.
+    INACTIVE : str
+        The hazard symbol is hidden from normal operations and disabled from use.
+    ADDED_MANUALLY : str
+        The hazard symbol was manually chosen.
+    """
+
     ACTIVE = "active"
     INACTIVE = "inactive"
     ADDED_MANUALLY = "Added Manually"
